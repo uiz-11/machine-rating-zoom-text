5352318f77018f3df5f2887143bcf9783cb3a74e
Yulin Zhang <yzhan114@ncsu.edu>
2021-03-18 16:47:54 -0400

    Updates

--- precache-manifest.0bc194ea2a22b56574b0b60a9acb8a10.js
+++ precache-manifest.0bc194ea2a22b56574b0b60a9acb8a10.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "21a9ec334a5dd859496929c42cd26933",
+    "revision": "0b12fe60de83fc1b8b334eaeb08c7e4f",
     "url": "/machine-rating-zoom-text/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/machine-rating-zoom-text/static/css/2.3cf5d70f.chunk.css"
   },
   {
-    "revision": "e0ea23f68a20337caa4b",
+    "revision": "be6d1296de2a3677ffda",
     "url": "/machine-rating-zoom-text/static/css/main.fcb87419.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/machine-rating-zoom-text/static/js/2.71c7f9e1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e0ea23f68a20337caa4b",
-    "url": "/machine-rating-zoom-text/static/js/main.d0d04d1e.chunk.js"
+    "revision": "be6d1296de2a3677ffda",
+    "url": "/machine-rating-zoom-text/static/js/main.e34c61af.chunk.js"
   },
   {
     "revision": "6d4c71de8aa921437083",
